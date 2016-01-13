28fad104e2cdf752d269832ce6a2efd60182074c
JAremko <w3techplayground@gmail.com>
2016-01-13 23:33:26 +0200

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git config --global user.email $GEMAIL
-git config --global user.name $GNAME
+git config --global user.email $GITEMAIL
+git config --global user.name $GITNAME
 
 export NO_AT_BRIDGE=1
 export SHELL=/usr/bin/fish
