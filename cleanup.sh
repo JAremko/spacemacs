fd9ae3f65618e5cc5040cbb7659a173fce4d68dd
Eugene Yaremenko <w3techplayground@gmail.com>
2016-02-20 01:41:22 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -3,8 +3,6 @@
 apt-get clean -y
 rm -rf /var/lib/apt/lists/*
 rm -rf /var/lib/apt/lists/partial/*
-apt-get clean -y
-apt-get update -y
-apt-get upgrade -y
+rm -rf /tmp/*
 
 find / -name ".git" -prune -exec rm -rf "{}" \;
