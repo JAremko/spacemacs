0ec9f50b0fb7e23752caa5693725ef817eb6b87f
Eugene Yaremenko <w3techplayground@gmail.com>
2016-03-16 21:18:26 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,5 +4,5 @@ apt-get clean -y
 rm -rf /var/lib/apt/lists/*
 rm -rf /var/lib/apt/lists/partial/*
 rm -rf /tmp/*
-
+#Also prevents Spacemacs form updating.
 find / -name ".git" -prune -exec rm -rf "{}" \;
