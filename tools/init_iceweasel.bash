80ec2ff271f1f4c5ee018d7982220e1fc0b52360
Eugene Yaremenko <w3techplayground@gmail.com>
2016-03-26 22:43:19 +0200

    Update init_iceweasel.bash

--- tools/init_iceweasel.bash
+++ tools/init_iceweasel.bash
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 sudo apt-get -y install xvfb
+echo "star Xvfb"
 sudo Xvfb :1 -screen 0 800x600x24&
+echo "sleep 10"
 sleep 10
+echo "start iceweasel"
 DISPLAY=:1 iceweasel&
+echo "sleep 60"
 sleep 60
+echo "kill Xvfb"
 sudo pkill Xvfb
+echo "kill iceweasel"
 sudo pkill -f iceweasel
 sudo apt-get -y purge xvfb
-
-return 0
+echo "exit 0"
+exit 0
