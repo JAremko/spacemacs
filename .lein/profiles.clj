c1039fcf8f6fcc76016f86207e72ff4c74e3dc3f
JAremko <w3techplayground@gmail.com>
2018-09-27 13:19:57 +0300

    add lein-bikeshed

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[jonase/eastwood "0.2.9"]
                   [lein-midje "3.2.1"]
                   [lein-kibit "0.1.6"]
+                  [lein-bikeshed "0.5.1"]
                   [venantius/yagni "0.1.4"]
                   [lein-pprint "1.2.0"]]}}
